1d88006716e93b38f8455352074ede0de0291b01
Skorikov Kirill <skorikovkr@gmail.com>
2023-11-05 17:35:17 +0500

    Форма пользователя

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -1,27 +1,65 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Typography } from "antd";
+import { Button, Form, Input } from "antd";
 
 import { useEffect, useState } from "react";
 
-import { userApi } from "@api/user/user.api";
-import { Role } from "@interfaces/user";
+import { ProfileInfoDto, userApi } from "@api/user/user.api";
+//import { Role } from "@interfaces/user";
+
+import styles from "./MyProfile.module.scss";
 
 export const MyProfile = () => {
-  const [profileData, setProfileData] = useState<any>();
+  const [profileData, setProfileData] = useState<ProfileInfoDto>();
+
+  const formItemLayout = { labelCol: { span: 4 }, wrapperCol: { span: 14 } };
+  const buttonItemLayout = { wrapperCol: { span: 14, offset: 4 } };
 
   useEffect(() => {
-    userApi
-      .getMyProfile({ role: Role.Applicants })
-      .then((data) => {
-        setProfileData(data);
-      })
-      .catch();
+    // userApi
+    //   .getMyProfile({ role: Role.Applicants })
+    //   .then((data) => {
+    //     setProfileData(data);
+    //   })
+    //   .catch();
+    const mockProfile: ProfileInfoDto = {
+      user_id: 1,
+      status_id: 0,
+      user: {
+        id: 1,
+        email: "user1@example.com",
+        first_name: "user_first_name",
+        last_name: "user_last_name",
+        username: "user1_username"
+      }
+    };
+    setProfileData(mockProfile);
   }, []);
 
   return (
-    <div>
-      <Typography>Тут будет информация о пользователе</Typography>
-      {profileData?.user?.email}
-    </div>
+    <Form
+      {...formItemLayout}
+      layout={"horizontal"}
+      initialValues={{ layout: "horizontal" }}
+      className={styles["authorization"]}
+    >
+      <Form.Item label="Имя">
+        <Input value={profileData?.user.first_name} />
+      </Form.Item>
+      <Form.Item label="Фамилия">
+        <Input value={profileData?.user.last_name} />
+      </Form.Item>
+      <Form.Item label="Почта">
+        <Input value={profileData?.user.email} readOnly />
+      </Form.Item>
+      <Form.Item label="Образование">
+        <Input placeholder="Soon..." />
+      </Form.Item>
+      <Form.Item label="О себе">
+        <Input />
+      </Form.Item>
+      <Form.Item {...buttonItemLayout}>
+        <Button type="primary">Сохранить</Button>
+      </Form.Item>
+    </Form>
   );
 };
