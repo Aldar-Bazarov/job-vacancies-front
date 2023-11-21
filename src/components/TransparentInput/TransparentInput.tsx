1e7824b3d99c9639561301845d96f16a2921092f
Aldar Bazarov <a.bazarov.sas@gmail.com>
2023-11-21 15:29:41 +0500

    Сверстал по макету авторизацию регистрацию

--- src/components/TransparentInput/TransparentInput.tsx
+++
@@ -1,9 +0,0 @@
-import { Input, InputProps, InputRef } from "antd";
-
-import styles from "./TransparentInput.module.scss";
-
-type TransparentInputProps = InputProps & React.RefAttributes<InputRef>;
-
-export const TransparentInput: React.FC<TransparentInputProps> = (props) => {
-  return <Input className={styles["input-transparent"]} {...props} />;
-};
--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,11 +1,10 @@
-import { Button, Form, Radio, Space, Typography, message } from "antd";
+import { Button, Form, Input, Radio, Typography, message } from "antd";
 
 import { useCallback, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { userApi } from "@api/user/user.api";
 import { Loader } from "@components/Loader/Loader";
-import { TransparentInput } from "@components/TransparentInput/TransparentInput";
 
 import styles from "./Registration.module.scss";
 
@@ -31,107 +30,141 @@ export const Registration: React.FC = () => {
 
   return (
     <div className={styles["registration"]}>
-      <Form
-        form={registrationForm}
-        colon={false}
-        labelWrap
-        size="small"
-        labelAlign="left"
-        className={styles["registration-form"]}
-        layout={"vertical"}
-        onFinish={onFinish}
-      >
-        <Typography.Title>Регистрация</Typography.Title>
-        <Space direction="vertical">
-          <Typography>Если у вас уже есть аккаунт</Typography>
-          <Typography>
-            Вы можете <a>Войти здесь!</a>
-          </Typography>
-        </Space>
-        <Form.Item
-          label="Имя"
-          name="firstName"
-          rules={[{ required: true, message: "Поле имя обязательно!" }]}
-        >
-          <TransparentInput placeholder="Введите имя" />
-        </Form.Item>
-        <Form.Item
-          label="Фамилия"
-          name="lastName"
-          rules={[{ required: true, message: "Поле фамилия обязательно!" }]}
-        >
-          <TransparentInput placeholder="Введите фамилию" />
-        </Form.Item>
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              type: "email",
-              message: "Введите корректный email!"
-            },
-            {
-              required: true,
-              message: "Поле Email обязательно!"
-            }
-          ]}
-        >
-          <TransparentInput placeholder="Введите почту" type="email" />
-        </Form.Item>
-        <Form.Item
-          label="Логин"
-          name="username"
-          rules={[{ required: true, message: "Поле логин обязательно!" }]}
-        >
-          <TransparentInput placeholder="Введите логин" />
-        </Form.Item>
-        <Form.Item
-          label="Пароль"
-          name="password"
-          rules={[{ required: true, message: "Поле пароль обязательно!" }]}
-        >
-          <TransparentInput type="password" placeholder="Введите пароль" />
-        </Form.Item>
-        <Form.Item
-          label="Повторите пароль"
-          name="confirmPassword"
-          dependencies={["password"]}
-          hasFeedback
-          rules={[
-            { required: true, message: "Повторите пароль!" },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(new Error("Пароли не совпадают!"));
-              }
-            })
-          ]}
-        >
-          <TransparentInput type="password" placeholder="Повторите пароль" />
-        </Form.Item>
-        <Form.Item
-          name="role"
-          rules={[{ required: true, message: "Поле обязательно!" }]}
-        >
-          <Radio.Group>
-            <Radio value={"applicants"}>Ищу работу</Radio>
-            <Radio value={"recruiters"}>Предлагаю работу</Radio>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item>
-          <Button
-            block
-            type="primary"
-            htmlType="submit"
-            className={styles["registration-form-button"]}
-          >
-            Зарегистрироваться
-          </Button>
-        </Form.Item>
-      </Form>
-      <Loader active={loading} />
+      <div className={styles["circle"]} id={styles["first"]}>
+        <div className={styles["circle"]} id={styles["second"]}>
+          <div className={styles["circle"]} id={styles["third"]}>
+            <Form
+              form={registrationForm}
+              colon={false}
+              labelWrap
+              size="small"
+              labelAlign="left"
+              className={styles["registration-form"]}
+              layout={"vertical"}
+              onFinish={onFinish}
+            >
+              <Link to="/">
+                <Typography.Title className={styles["logo"]}>
+                  VPI
+                </Typography.Title>
+              </Link>
+              <Typography>Поиск вакансий программная инженерия</Typography>
+              <Typography.Title level={4}>Регистрация</Typography.Title>
+              <Form.Item
+                label="Имя"
+                name="firstName"
+                rules={[{ required: true, message: "Поле имя обязательно!" }]}
+              >
+                <Input
+                  placeholder="Введите имя"
+                  className={styles["registration-form-button"]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Фамилия"
+                name="lastName"
+                rules={[
+                  { required: true, message: "Поле фамилия обязательно!" }
+                ]}
+              >
+                <Input
+                  placeholder="Введите фамилию"
+                  className={styles["registration-form-button"]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    type: "email",
+                    message: "Введите корректный email!"
+                  },
+                  {
+                    required: true,
+                    message: "Поле Email обязательно!"
+                  }
+                ]}
+              >
+                <Input
+                  placeholder="Введите почту"
+                  type="email"
+                  className={styles["registration-form-button"]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Логин"
+                name="username"
+                rules={[{ required: true, message: "Поле логин обязательно!" }]}
+              >
+                <Input
+                  placeholder="Введите логин"
+                  className={styles["registration-form-button"]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Пароль"
+                name="password"
+                rules={[
+                  { required: true, message: "Поле пароль обязательно!" }
+                ]}
+              >
+                <Input
+                  type="password"
+                  placeholder="Введите пароль"
+                  className={styles["registration-form-button"]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Повторите пароль"
+                name="confirmPassword"
+                dependencies={["password"]}
+                hasFeedback
+                rules={[
+                  { required: true, message: "Повторите пароль!" },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue("password") === value) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(new Error("Пароли не совпадают!"));
+                    }
+                  })
+                ]}
+              >
+                <Input
+                  type="password"
+                  placeholder="Повторите пароль"
+                  className={styles["registration-form-button"]}
+                />
+              </Form.Item>
+              <Form.Item
+                name="role"
+                rules={[{ required: true, message: "Поле обязательно!" }]}
+              >
+                <Radio.Group>
+                  <Radio value={"applicants"}>Ищу работу</Radio>
+                  <Radio value={"recruiters"}>Предлагаю работу</Radio>
+                </Radio.Group>
+              </Form.Item>
+              <Form.Item>
+                <Button
+                  block
+                  type="primary"
+                  htmlType="submit"
+                  className={styles["registration-form-button"]}
+                >
+                  Зарегистрироваться
+                </Button>
+              </Form.Item>
+              <Typography>
+                Уже есть аккаунт? <Link to={"/auth"}>Авторизоваться</Link>
+              </Typography>
+            </Form>
+            <Loader active={loading} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
