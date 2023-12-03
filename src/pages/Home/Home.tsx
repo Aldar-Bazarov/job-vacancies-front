1992f102a4637ac8ddc13a902da3548ea19aec4d
Skorikov Kirill <skorikovkr@gmail.com>
2023-12-03 15:08:59 +0500

    изменил Company

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,8 @@ import { Typography, Row, Col, Flex, message } from "antd";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { companiesApi } from "@api/companies/companies.api";
-import { CompanyInfo } from "@api/companies/types";
+import { companyApi } from "@api/company/company.api";
+import { CompanyInfoDto } from "@api/company/types";
 import { searchApi } from "@api/search/search.api";
 import { Search } from "@components/Search/Search";
 import { isAuthenticated } from "@infrastructure/axios/auth";
@@ -15,7 +15,7 @@ import mainLogo from "./MainLogo.png";
 
 export const Home: React.FC = () => {
   const [isAuth, setAuth] = useState(isAuthenticated());
-  const [companies, setCompanies] = useState<CompanyInfo[]>([]);
+  const [companies, setCompanies] = useState<CompanyInfoDto[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
   const navigate = useNavigate();
 
@@ -24,7 +24,7 @@ export const Home: React.FC = () => {
   }, [isAuth]);
 
   useEffect(() => {
-    companiesApi
+    companyApi
       .getCompanies()
       .then((data) => {
         setCompanies(data.length < 7 ? data : data.slice(0, 6));
