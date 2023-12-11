cc2ef2d1fc64ef792f203eb834798118c24df538
Мышкин Даниил <dsmyshkin@asuproject.ru>
2023-12-12 00:50:22 +0500

    Оказывается, перепутаны вакансии и компании, я это заметил слишком поздно, исправлять пока не хочу, исправлю, когда хотя бы один раз смогу сохранить комапнию и вакансию в нашу замечательную базу без танцев с бубнами. Писать по тыще раз мок объекты желание отсутствует! Останется допилисть отображаему информацию в соответствии с моделями на беке, а не с воображением дизайнера. Также осталось добавить развернутую вакансию, но по причинам, которые я описал выше, делать это сейчас нецелесообразно. Надеюсь, наши базы снова встанут, статусы запросов уменьшатся на 300 и станут ровно 200, а трава будет зеленая и лимонад вкусный!

--- src/components/Vacancies/Vacancies.tsx
+++ src/components/Vacancies/Vacancies.tsx
@@ -1,39 +1,49 @@
-import { Button, Col, Flex, Pagination, Row, Typography } from "antd";
+import { Col, Row } from "antd";
 
 import { useState, useEffect } from "react";
 
 import { VacancyInfo } from "@api/vacancies/types";
-import { vacanciesApi } from "@api/vacancies/vacancies.api";
+// import { vacanciesApi } from "@api/vacancies/vacancies.api";
+import { CustomPagination } from "@components/CustomPagination/Pagination";
 import { Search } from "@components/Search/Search";
 
+import data from "./mock-vacancies.json";
 import styles from "./Vacancies.module.scss";
 import { VacancyCard } from "./VacancyCard";
-
-const DEFAULT_PAGE_SIZE = 6;
 
 export const Vacancies = () => {
   const [inputValue, setInputValue] = useState<string>("");
   const [vacancies, setVacancies] = useState<VacancyInfo[]>([]);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [totalCount, setTotalCount] = useState<number>(1);
+  // const [currentPage, setCurrentPage] = useState<number>(1);
+  // const [totalCount, setTotalCount] = useState<number>(1);
 
   const handleSearch = () => {
     //TODO сделать поиск по компании. Опять же непонятно, а как именно искать то?
   };
 
-  useEffect(() => {
-    vacanciesApi.getVacancies().then((data) => {
-      setTotalCount(data.length);
-      setVacancies(data);
-    });
-  }, []);
-
-  const handleDownload = () => {
-    setCurrentPage((prev) => prev + 1);
+  const handleChangePage = () => {
+    //TODO а нам нужны уже какие-то данные собственно для этого
   };
 
+  useEffect(() => {
+    setVacancies(
+      data.map((v) => ({ ...v, created_at: new Date(v.created_at) }))
+    );
+  }, []);
+
+  // useEffect(() => {
+  //   vacanciesApi.getVacancies().then((data) => {
+  //     setTotalCount(data.length);
+  //     setVacancies(data);
+  //   });
+  // }, []);
+
+  // const handleDownload = () => {
+  //   setCurrentPage((prev) => prev + 1);
+  // };
+
   return (
-    <>
+    <div className={styles["vacancies"]}>
       <Search
         handleSearch={handleSearch}
         inputPlaceholder={"Введите вакансию..."}
@@ -42,35 +52,21 @@ export const Vacancies = () => {
       />
       <hr className={styles["hr"]} />
       <Row gutter={[32, 32]}>
-        {vacancies.map(
-          (el, index) =>
-            index < DEFAULT_PAGE_SIZE * currentPage && (
-              <Col span={12} key={el.id}>
-                <VacancyCard
-                  key={el.id}
-                  company_id={el.company_id}
-                  description={el.description}
-                  id={el.id}
-                />
-              </Col>
-            )
-        )}
+        {vacancies.map((el) => (
+          <Col span={12} key={el.id}>
+            <VacancyCard
+              key={el.id}
+              company_id={el.company_id}
+              description={el.description}
+              id={el.id}
+            />
+          </Col>
+        ))}
       </Row>
-      <Flex
-        justify="center"
-        align="center"
-        className={styles["button-container"]}
-      >
-        <Button
-          disabled={
-            (currentPage + 1) * DEFAULT_PAGE_SIZE >
-            totalCount + DEFAULT_PAGE_SIZE
-          }
-          onClick={handleDownload}
-        >
-          Загрузить еще
-        </Button>
-      </Flex>
-    </>
+      <CustomPagination
+        total={data.length + 500}
+        handleSearch={handleChangePage}
+      />
+    </div>
   );
 };
--- src/components/Vacancies/VacancyCard.tsx
+++ src/components/Vacancies/VacancyCard.tsx
@@ -2,9 +2,10 @@ import { Card, Flex, Typography } from "antd";
 
 import { useMemo, useState, useEffect } from "react";
 
-import { companyApi } from "@api/company/company.api";
+// import { companyApi } from "@api/company/company.api";
 import { CompanyInfoDto } from "@api/company/types";
 
+import mock from "./mock-vacancy-card.json";
 import styles from "./VacancyCard.module.scss";
 
 interface IVacancyCardProps {
@@ -13,21 +14,29 @@ interface IVacancyCardProps {
   company_id: number;
 }
 
-export const VacancyCard = ({
-  id,
-  company_id,
-  description
-}: IVacancyCardProps) => {
+export const VacancyCard = ({ company_id, description }: IVacancyCardProps) => {
   const [companyInfo, setCompanyInfo] = useState<CompanyInfoDto>();
 
   useEffect(() => {
-    companyApi.getCompany(company_id).then((data) => setCompanyInfo(data));
-  }, [company_id]);
+    // companyApi.getCompany(company_id).then((data) => setCompanyInfo(data));
+    setCompanyInfo({
+      ...mock,
+      created_at: new Date(mock.created_at),
+      description,
+      vacancies: mock.vacancies.map((v) => ({
+        ...v,
+        created_at: new Date(v.created_at)
+      }))
+    });
+  }, [company_id, description]);
 
   const titleComponent = useMemo(() => {
     return (
-      <Flex justify="space-between" align="center">
-        <Typography.Title level={3}>{companyInfo?.name}</Typography.Title>
+      <Flex justify="start" align="center" gap={30}>
+        <img src={companyInfo?.logo} width={50} height={50} />
+        <Typography.Title level={3} style={{ margin: 0 }}>
+          {companyInfo?.name}
+        </Typography.Title>
       </Flex>
     );
   }, [companyInfo]);
