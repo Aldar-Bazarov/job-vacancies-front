fd7bb0526985186f305a01675c6032f07e12c500
Мышкин Даниил <dsmyshkin@asuproject.ru>
2023-12-12 00:15:38 +0500

    full create vacancy page, needs to add query:

--- src/components/CreateVacancy/CreateVacancy.tsx
+++ src/components/CreateVacancy/CreateVacancy.tsx
@@ -1,35 +1,122 @@
-import { Form, Input, InputNumber, Select, Typography } from "antd";
+import {
+  Button,
+  Col,
+  Flex,
+  Form,
+  Input,
+  InputNumber,
+  Row,
+  Typography
+} from "antd";
+import TextArea from "antd/es/input/TextArea";
 
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { EditableFormItem } from "@components/EditableFormItem/EditableFormItem";
+import { HardSkills } from "@pages/Profile/Profile.Skills";
+
+import styles from "./CreateVacancy.module.scss";
 
 export const CreateVacancy = (): JSX.Element => {
   const [createVacancyForm] = Form.useForm();
+  const [tags, setTags] = useState(["Unit-тестирование", "Долгие созвоны"]);
+  const navigate = useNavigate();
+
+  const handleCreateVacancy = async () => {
+    console.log({
+      ...createVacancyForm.getFieldsValue(),
+      personal_qualities: tags.join(",")
+    });
+  };
 
   return (
     <Form form={createVacancyForm}>
-      <Typography>Создание вакансии</Typography>
-      <Form.Item
-        label="Название вакансии"
-        name="name"
-        rules={[{ required: true, message: "Поле имя обязательно!" }]}
-      >
-        <Input placeholder="Введите название вакансии..." />
-      </Form.Item>
-      <Form.Item label="Зарплата" name="salary_min">
-        <InputNumber placeholder="От..." />
-      </Form.Item>
-      <Form.Item name="salary_max">
-        <InputNumber placeholder="До..." />
-      </Form.Item>
-      <Form.Item label="Опыт" name="experience_min">
-        <InputNumber placeholder="От..." />
-      </Form.Item>
-      <Form.Item name="experience_max">
-        <InputNumber placeholder="До..." />
-      </Form.Item>
-      <Form.Item label="Требования" name="description">
-        <InputNumber placeholder="Введите требования для вакансии..." />
-      </Form.Item>
+      <Typography.Title>Создание вакансии</Typography.Title>
+      <Row>
+        <Col span={12}>
+          <Typography.Title level={4}>Название вакансии</Typography.Title>
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: "Поле имя обязательно!" }]}
+          >
+            <Input
+              placeholder="Введите название вакансии..."
+              className={styles["big-input"]}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Typography.Title level={4}>Зарплата ₽</Typography.Title>
+          <Row gutter={[8, 8]}>
+            <Col span={6}>
+              <Form.Item name="salary_min">
+                <InputNumber
+                  placeholder="От..."
+                  className={styles["number-input"]}
+                />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item name="salary_max">
+                <InputNumber
+                  placeholder="До..."
+                  className={styles["number-input"]}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Col span={12}>
+        <Typography.Title level={4}>Опыт</Typography.Title>
+        <Row gutter={[8, 8]}>
+          <Col span={6}>
+            <Form.Item name="experience_min">
+              <InputNumber
+                placeholder="От..."
+                className={styles["number-input"]}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item name="experience_max">
+              <InputNumber
+                placeholder="До..."
+                className={styles["number-input"]}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={24}>
+        <Typography.Title level={4}>Требования</Typography.Title>
+        <Form.Item name="description">
+          <TextArea
+            placeholder="Введите требования для вакансии..."
+            className={styles["textarea"]}
+          />
+        </Form.Item>
+      </Col>
+      <EditableFormItem icon={<></>} readonly={false} title="Проф навыки">
+        <HardSkills setTags={setTags} tags={tags} />
+      </EditableFormItem>
+      <Flex style={{ width: "100%" }} justify={"end"} align={"end"} gap={30}>
+        <Form.Item>
+          <Button
+            type="text"
+            size="large"
+            onClick={() => navigate("/", { replace: true })}
+          >
+            Отменить
+          </Button>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" size="large" onClick={handleCreateVacancy}>
+            Сохранить
+          </Button>
+        </Form.Item>
+      </Flex>
     </Form>
   );
 };
