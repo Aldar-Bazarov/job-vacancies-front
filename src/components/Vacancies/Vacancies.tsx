8baa0de42d8a23633708cdab11138b7c91d0e641
Мышкин Даниил <dsmyshkin@asuproject.ru>
2023-12-16 14:41:54 +0500

    add laoders

--- src/components/Vacancies/Vacancies.tsx
+++ src/components/Vacancies/Vacancies.tsx
@@ -8,6 +8,7 @@ import { VacancyInfo } from "@api/vacancies/types";
 import { vacanciesApi } from "@api/vacancies/vacancies.api";
 import { Card } from "@components/Card";
 import { CustomPagination } from "@components/CustomPagination/Pagination";
+import { Loader } from "@components/Loader/Loader";
 import { Search } from "@components/Search/Search";
 import { HardSkills } from "@pages/Profile/Profile.Skills";
 
@@ -18,9 +19,11 @@ export const Vacancies = () => {
   const [vacancies, setVacancies] = useState<VacancyInfo[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [total, setTotal] = useState<number>(0);
+  const [loading, setLoading] = useState(false);
 
   //Мне честно ок, мы уже все равно не можем ждать, пока они сделают норм поиск)))
   const handleSearch = async () => {
+    setLoading(true);
     let result: VacancyInfo[] = [];
 
     await searchApi
@@ -48,61 +51,70 @@ export const Vacancies = () => {
       })
       .then((data) => {
         result = result.concat(data.vacancies);
-      });
+      })
+      .finally(() => setLoading(false));
 
     setVacancies(result);
     setTotal(result.length);
   };
 
   useEffect(() => {
-    vacanciesApi.getVacancies().then((data) => {
-      setVacancies(data);
-    });
+    setLoading(true);
+    vacanciesApi
+      .getVacancies()
+      .then((data) => {
+        setVacancies(data);
+      })
+      .finally(() => setLoading(false));
   }, []);
 
   const handleChangePage = () => {
     setCurrentPage((prev) => prev + 1);
   };
 
-  //TODO сменить описание на имя бы
   return (
-    <div className={styles["vacancies"]}>
-      <Search
-        handleSearch={handleSearch}
-        inputPlaceholder={"Начните вводить название для поиска..."}
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-      />
-      <Divider />
-      <Row gutter={[32, 32]}>
-        {vacancies.map((el) => (
-          <Col span={12} key={el.id}>
-            <Link to={`/vacancies/${el.id}`}>
-              <Card imageSrc="">
-                <Card.Title>
-                  {el.name.length > 15 ? el.name.slice(0, 12) + "..." : el.name}
-                </Card.Title>
-                <Card.Title level="2">
-                  Оплата {el.salary_min} - {el.salary_max} тыс. руб
-                </Card.Title>
-                <Card.Title level="2">
-                  Опыт от {el.experience_min} до {el.experience_max} лет
-                </Card.Title>
-                <Card.Content>Занятость: {el.rate.name}</Card.Content>
-                <Card.Content>
-                  Требуемые навыки:
-                  <HardSkills tags={el.personal_qualities.split(",")} />
-                </Card.Content>
-              </Card>
-            </Link>
-          </Col>
-        ))}
-      </Row>
-      <CustomPagination
-        total={total}
-        handleSearch={(val: number) => setCurrentPage(val)}
-        current={currentPage}
-      />
-    </div>
+    <>
+      <Loader active={loading} />
+      <div className={styles["vacancies"]}>
+        <Search
+          handleSearch={handleSearch}
+          inputPlaceholder={"Начните вводить название для поиска..."}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+        />
+        <Divider />
+        <Row gutter={[32, 32]}>
+          {vacancies.map((el) => (
+            <Col span={12} key={el.id}>
+              <Link to={`/vacancies/${el.id}`}>
+                <Card imageSrc="">
+                  <Card.Title>
+                    {el.name.length > 15
+                      ? el.name.slice(0, 12) + "..."
+                      : el.name}
+                  </Card.Title>
+                  <Card.Title level="2">
+                    Оплата {el.salary_min} - {el.salary_max} тыс. руб
+                  </Card.Title>
+                  <Card.Title level="2">
+                    Опыт от {el.experience_min} до {el.experience_max} лет
+                  </Card.Title>
+                  <Card.Content>Занятость: {el.rate.name}</Card.Content>
+                  <Card.Content>
+                    Требуемые навыки:
+                    <HardSkills tags={el.personal_qualities.split(",")} />
+                  </Card.Content>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+        <CustomPagination
+          total={total}
+          handleSearch={(val: number) => setCurrentPage(val)}
+          current={currentPage}
+        />
+      </div>
+    </>
   );
 };
